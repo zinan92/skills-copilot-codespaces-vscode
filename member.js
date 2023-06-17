34ad0cf3a31c6a64aae7ca40c64f1df4e3b0c9ef
zinan92 <zinan92@hotmail.com>
2023-06-17 04:45:09 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberDiv = document.getElementById("memberDiv");
+    var skillsDiv = document.getElementById("skillsDiv");
+    var projectsDiv = document.getElementById("projectsDiv");
+    var contactDiv = document.getElementById("contactDiv");
+    var aboutDiv = document.getElementById("aboutDiv");
+    member.style.color = "#fff";
+    skills.style.color = "#000";
+    projects.style.color = "#000";
+    contact.style.color = "#000";
+    about.style.color = "#000";
+    memberDiv.style.display = "block";
+    skillsDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    contactDiv.style.display = "none";
+    aboutDiv.style.display = "none";
+}
